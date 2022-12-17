9c45d1db7a87b32eb306b42814ee5d74f74b9215
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-17 02:42:51 -0500

    Update memide.js

--- memide.js
+++ memide.js
@@ -1,9 +1,7 @@
-const user = urlParams.get('$(user)');
-
 if(x<=10) {
-    `A ${user} le mide ${x} cms. PoroSad `
+    `A $(user) le mide ${x} cms. PoroSad `
 } else if (x>=11 && x<=20){
-        `A ${user} le mide ${x} cms. PogChamp `
+        `A $(user) le mide ${x} cms. PogChamp `
 } else {
-    `A ${user} le mide ${x} cms. Kreygasm `
+    `A $(user) le mide ${x} cms. Kreygasm `
 }
