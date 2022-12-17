a52f58c6566699f43707630a1dc714d78c797aca
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-17 10:50:16 -0500

    Update suerte.js

--- twitch/suerte.js
+++ twitch/suerte.js
@@ -1,7 +1,5 @@
 if (q) {
     window.location.href;
-  //$(urlfetch json https://twitch.bayzamusic.com/suerte/$(touser))
 } else {
     window.location.href;
-  //$(urlfetch json https://twitch.bayzamusic.com/suerte/$(user))
 }
