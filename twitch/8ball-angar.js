07df1b7319dd9d03a5c5fbee3847dbd0575bea67
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-17 05:30:58 -0500

    Update 8ball-angar.js

--- twitch/8ball-angar.js
+++ twitch/8ball-angar.js
@@ -9,7 +9,7 @@ arr_respuestas = [
     `No lo sé. angarSad `|
     `Tal vez Angar sepa. angarL `|
     `Tal vez Tiqui sepa. angarJu`
-];
+]
 
 if (q) {
     respuesta = arr_respuestas[Math.floor(Math.random()*arr_respuestas.length)];
