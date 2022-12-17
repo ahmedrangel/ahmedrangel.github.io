5e388f4f20f5b890cd26d1d05079e034ed70d6cb
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-17 04:16:59 -0500

    Update 8ball.js

--- twitch/8ball.js
+++ twitch/8ball.js
@@ -1,10 +1,10 @@
-var arr_respuestas = [
+arr_respuestas = [
     `Es cierto angarSi `,
     `Tal vez angarMmmm `,
     `Claro que no angarNo `,
     `No entendí, vuelve a preguntar angarG `,
     `Es muy probable angarMonkas `,
     `Es poco probable angarW `
-] 
+];
 
 respuesta = arr_respuestas[Math.floor(Math.random()*arr_respuestas.length)];
