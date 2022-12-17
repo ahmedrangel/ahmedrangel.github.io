51f5faeafb3cf27ab835fa62057c43884e0113d0
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-17 01:42:06 -0500

    Update memide.js

---
+++ memide.js
@@ -0,0 +1,7 @@
+if(x<=10) {
+    "A $(user) le mide $(eval x) cms. PoroSad "
+} else if (x>=11 && x<=20){
+        "A $(user) le mide $(eval x) cms. PogChamp "
+} else {
+    "A $(user) le mide $(eval x) cms. Kreygasm "
+};
