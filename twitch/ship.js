a3f7b8be31f58ca3258457441c23203e6ec8fe9a
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-19 00:23:48 -0500

    Update ship.js

--- twitch/ship.js
+++ twitch/ship.js
@@ -3,7 +3,6 @@ ship = `${split[0]} y ${split[1]}`;
 letras_nombre1 = split[0].substring(0, 3);
 letras_nombre2 = split[1].substring(split[1].length - 2);
 nombre_ship = `${letras_nombre1}${letras_nombre2}`
-porcentaje = Math.round(Math.random()*100);
 if (split.length == 2) {
 	`${ship} tienen una compatibilidad de ${porcentaje}%. El nombre del ship es: ${nombre_ship}`;
 }
