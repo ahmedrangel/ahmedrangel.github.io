208a5b9fd3cabfdc85ad843821ae6221a48b68e6
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-19 02:43:29 -0500

    Update ship.js

--- twitch/ship.js
+++ twitch/ship.js
@@ -1,10 +1,25 @@
+porcentaje = Math.round(Math.random()*100);
+emote_adpt = ``;
+if (porcentaje >= 90){
+    emote_adpt = `angarH `;
+} else if (porcentaje >= 70 && porcentaje < 90) {
+    emote_adpt = `angarShy `;
+} else if (porcentaje >= 50 && porcentaje < 70) {
+    emote_adpt = `angarJu `;
+} else if (porcentaje >= 30 && porcentaje < 50) {
+    emote_adpt = `angarG `;
+} else if (porcentaje >= 10 && porcentaje < 30) {
+    emote_adpt = `angarSadge `;
+} else if (porcentaje >= 0 && porcentaje < 10) {
+    emote_adpt = `angarSad `;
+}
+
 if (q) {
     user = u;
     touser = t;
     query = q;
     query = query.replace(/@/g, '');
     espacios = query.split(" ").length - 1;
-    porcentaje = Math.round(Math.random()*100);
     mensaje = null;
     if (espacios > 0 && espacios <= 1) {
         split = query.split(" ");
@@ -13,22 +28,25 @@ if (q) {
         letras_nombre2 = split[1].substring(split[1].length - 2);
         nombre_ship = `${letras_nombre1}${letras_nombre2}`;
         if (split.length == 2) {
-            mensaje = `${ship} son ${porcentaje}% compatibles. ❤️ El nombre del ship es: ${nombre_ship}. angarH `;
+            mensaje = `${ship} son ${porcentaje}% compatibles. ❤️ El nombre del ship es: ${nombre_ship}. ${emote_adpt}`;
         }
         mensaje;
     } else if (espacios >= 2) {
-        mensaje = `${u}, No tengo la capacidad de calcular la compatibilidad con más de 2 personas. angarG `;
+        emote_adpt = `angarG `;
+        mensaje = `${u}, No tengo la capacidad de calcular la compatibilidad con más de 2 personas. ${emote_adpt}`;
     } else if (user.toLowerCase() == touser.toLowerCase()) {
-        mensaje = `${u}, Acaso estas tratando de ver si eres compatible contigo mismo? angarMonkas`;
+        emote_adpt = `angarMonkas `;
+        mensaje = `${u}, Acaso estas tratando de ver si eres compatible contigo mismo? ${emote_adpt}`;
     } else {
         ship = `${u} y ${t}`;
         letras_nombre1 = u.substring(0, 3);
         letras_nombre2 = t.substring(t.length - 2);
         nombre_ship = `${letras_nombre1}${letras_nombre2}`;
-        mensaje = `${ship} son ${porcentaje}% compatibles. ❤️ El nombre del ship es: ${nombre_ship}. angarH `;
+        mensaje = `${ship} son ${porcentaje}% compatibles. ❤️ El nombre del ship es: ${nombre_ship}. ${emote_adpt}`;
         mensaje;
     }
 } else {
-    mensaje = `${u}, Menciona a alguien para descubrir tu compatibilidad o otras dos personas para saber la de ellos. angarJu `;
+    emote_adpt = `angarJu `;
+    mensaje = `${u}, Menciona a alguien para descubrir tu compatibilidad o otras dos personas para saber la de ellos. ${emote_adpt}`;
     mensaje;
 }
