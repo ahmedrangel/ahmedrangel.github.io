4c24a0223d83bad53042b6e811533461f4c0bf28
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2022-12-20 18:36:41 -0500

    Update 8ball-angar.js

--- twitch/8ball-angar.js
+++ twitch/8ball-angar.js
@@ -1,18 +1,20 @@
 arr_respuestas = [
-    `Es cierto. angarSi `,
-    `Por supuesto! angarP `,
-    `Puede ser. angarMmm `,
-    `Claro que no. angarNo `,
-    `No entendí, vuelve a preguntar. angarG `,
-    `Es muy probable. angarMonkas `,
-    `Es poco probable. angarW `,
-    `No lo sé. angarSad `,
-    `Tal vez Angar sepa. angarL `,
+    `Es cierto. angarSi`,
+    `Por supuesto! angarP`,
+    `Puede ser. angarMmm`,
+    `No... angarNo`,
+    `Claro que no. angarNo`,
+    `No entendí, vuelve a preguntar. angarG`,
+    `Es muy probable. angarMonkas`,
+    `Es poco probable. angarW`,
+    `Sí, definitivamente. angarSi`,
+    `No lo sé. angarSad`,
+    `Tal vez Angar sepa. angarL`,
     `Tal vez Tiqui tenga la respuesta. angarJu`
 ];
 
 if (q) {
     respuesta = arr_respuestas[Math.floor(Math.random()*arr_respuestas.length)];
 } else {
-    `Escribe una pregunta. angarMad Las respuestas de este comando mayormente son para afirmar o negar una pregunta. angarPls `;
+    `Escribe una pregunta. angarMad Las respuestas de este comando mayormente son para afirmar o negar una pregunta. angarPls`;
 }
