a39902fa5e7afd6bdd5fdf1ea4fbc08213f4e557
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-01-28 20:37:16 -0500

    Create 8ball.js

---
+++ twitch/8ball.js
@@ -0,0 +1,19 @@
+arr_respuestas = [
+    `Es cierto. angarSi`,
+    `Por supuesto! FallWinning`,
+    `Puede ser. BegWan`,
+    `No... angarNo`,
+    `Claro que no. angarNo`,
+    `No entendí, vuelve a preguntar. CoolStoryBob `,
+    `Es muy probable. Shush`,
+    `Es poco probable. FallHalp`,
+    `Sí, definitivamente. angarSi`,
+    `No lo sé. TearGlove`,
+    `Tal vez Joss tenga la respuesta. PotFriend`
+];
+
+if (q) {
+    respuesta = arr_respuestas[Math.floor(Math.random()*arr_respuestas.length)];
+} else {
+    `Escribe una pregunta. angarMad Las respuestas de este comando mayormente son para afirmar o negar una pregunta. PotFriend`;
+}
