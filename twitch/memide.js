636eecf3762ba440c728348278ff9c60f30905ca
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-03 21:09:40 -0500

    Update memide.js

--- twitch/memide.js
+++ twitch/memide.js
@@ -1,7 +1,7 @@
 if(x<=9) {
-    `A ${u} le mide ${x} cms. PoroSad `;
+    `A ${u} le mide ${x} cms. zihneeTite`;
 } else if (x>=10 && x<=15){
-        `A ${u} le mide ${x} cms. PogChamp `;
+        `A ${u} le mide ${x} cms. zihneePog`;
 } else {
-    `A ${u} le mide ${x} cms. Kreygasm `;
+    `A ${u} le mide ${x} cms. zihneeUwu`;
 }
