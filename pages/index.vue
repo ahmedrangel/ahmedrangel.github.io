6887b3db054bb1995c1b33c590df9b2e4c37fb1a
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-25 04:31:18 -0500

    fix: skills for

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,8 @@ const projectsOrdered = computed(() => {
 });
 
 const dark = useCookie("dark");
+
+console.log(SKILLS);
 </script>
 
 <template>
@@ -93,7 +95,7 @@ const dark = useCookie("dark");
               <span class="fw-bold mb-0 text-color-inverted">{{ t(main_skills.skill_id) }}</span>
             </td>
             <td>
-              <template v-for="(skills, index2) of SKILLS.skills" :key="index2">
+              <template v-for="(skills, index2) of main_skills.skills" :key="index2">
                 <div class="d-inline-flex" data-aos="zoom-in">
                   <span class="sk m-1 px-2 py-1 text-nowrap rounded-2" :class="dark ? 'bg-dark-3' : 'bg-white-3'">
                     <Icon class="iconify me-1" :name="skills.icon" size="1.2em" />{{ skills.name }}
