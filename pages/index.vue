47c09c2b61deb737fa9369817d0de5c39c6b3d59
Ahmed Rangel <ahmedrangel@outlook.com>
2024-07-17 23:24:38 -0500

    chore: use one property for project images

--- pages/index.vue
+++ pages/index.vue
@@ -117,8 +117,8 @@ watch(() => {
       <MasonryWall :items="projectsOrdered" :ssr-columns="1" :gap="12" :max-columns="2" :column-width="400" data-aos="fade-in">
         <template #default="{ item: projects }">
           <div class="card overflow-hidden">
-            <img v-if="projects.image" :src="`/images/${projects.image}`" class="card-img-top" alt="" data-aos="fade-in">
-            <div v-if="projects.images" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <img v-if="projects.images.length === 1" :src="`/images/${projects.images[0]}`" class="card-img-top" alt="" data-aos="fade-in">
+            <div v-else class="carousel slide carousel-fade" data-bs-ride="carousel">
               <div class="carousel-indicators mb-2">
                 <div class="px-2 rounded-2 d-flex" :class="dark ? 'bg-dark' : 'bg-light'">
                   <button v-for="(image, i) of projects.images" :key="i" type="button" data-bs-target=".carousel" :data-bs-slide-to="i" :class="{ 'active': !i, 'bg-dark': !dark, 'bg-white': dark }" aria-current="true" aria-label="Slide" />
