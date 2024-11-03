5e7b3e0a719a107e46391998c87508163d03d0e7
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-03 05:08:21 -0500

    chore: add webhook skill

--- app/utils/skills.js
+++ app/utils/skills.js
@@ -81,7 +81,8 @@ export const SKILLS = [
       { name: "Caching", icon: "ic:baseline-cached" },
       { name: "SSH/SFTP", icon: "ic:baseline-folder" },
       { name: "Redirects", icon: "ic:baseline-next-plan" },
-      { name: "WebSockets", icon: "ic:baseline-web-asset" }
+      { name: "WebSockets", icon: "ic:baseline-web-asset" },
+      { name: "Webhooks", icon: "ic:round-webhook" }
     ]
   },
   {
