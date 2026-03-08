e97c613c73979dc5819ee168ed7000da348b6457
Ahmed Rangel <ahmedrangel@outlook.com>
2026-03-08 13:01:55 -0500

    fix: deprecated error props

--- app/error.vue
+++ app/error.vue
@@ -1,13 +1,18 @@
-<script setup>
-const { statusCode, statusMessage, message } = useError().value;
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{
+  error: NuxtError;
+}>();
+
 useHead({ htmlAttrs: { "data-bs-theme": "light" } });
 </script>
 
 <template>
   <div class="bg-primary vh-100 d-flex justify-content-center align-items-center text-white">
     <div class="bg-dark p-5 rounded-3 shadow">
-      <h1>Error {{ statusCode }}</h1>
-      <h5>{{ "error_occurred" }}: {{ message || statusMessage }}</h5>
+      <h1>Error {{ error.status }}</h1>
+      <h5>{{ "error_occurred" }}: {{ error.message || error.statusText || "Error" }}</h5>
       <p>{{ "go_back" }}: <NuxtLink to="/" class="text-decoration-underline text-white">link</NuxtLink></p>
     </div>
   </div>
